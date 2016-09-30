8de04424f01ea3f38da31e3f1672d870b4065e73
Taylor Burmeister <burmeister.taylor@gmail.com>
2016-09-29 23:10:20 -0400

    first commit - started working on templates and copied over some stuff from Pyrobuf

---
+++ cyrobuf/__main__.py
@@ -0,0 +1,86 @@
+import re
+import os
+import sys
+import glob
+import argparse
+from distutils.core import setup
+
+from jinja2 import Environment, PackageLoader
+
+from pyrobuf.parse_proto import Parser
+
+
+HERE = os.path.dirname(os.path.abspath(__file__))
+
+def main():
+    args = cli_argument_parser()
+    gen_message(args.source, out=args.out_dir, build=args.build_dir, install=args.install)
+
+def cli_argument_parser():
+    parser = argparse.ArgumentParser("cyrobuf", description="a Cython based protobuf compiler")
+    parser.add_argument('source', type=str,
+                        help="filename.proto or directory containing proto files")
+    parser.add_argument('--out-dir', default='out',
+                        help="output directory [default: out]")
+    parser.add_argument('--build-dir', default='build',
+                        help="C compiler build directory [default: build]")
+    parser.add_argument('--install', action='store_true',
+                        help="install the extension [default: False]")
+    return parser.parse_args()
+
+def gen_message(fname, out="out", build="build", install=False):
+
+    parser = Parser()
+
+    env = Environment(loader=PackageLoader('cyrobuf.protobuf', 'templates'))
+    templ_h = env.get_template('proto_h.tmpl')
+    templ_c = env.get_template('proto_c.tmpl')
+
+    try:
+        os.makedirs(out)
+    except:
+        pass
+
+    script_args = ['build', '--build-base={0}'.format(build)]
+    if install:
+        script_args.append('install')
+
+    if os.path.isdir(fname):
+        for spec in glob.glob(os.path.join(fname, '*.proto')):
+            generate(spec, out, parser, templ_h, templ_c)
+
+        _, name = os.path.split(fname)
+        c = os.path.join(out, '*.c')
+
+    else:
+        name, _ = os.path.splitext(os.path.basename(fname))
+        if not name:
+            print("not a .proto file")
+            return
+
+        generate(fname, out, parser, templ_h, templ_c)
+
+        c = os.path.join(out, "%s_proto.c" % name)
+
+    setup(name=name, script_args=script_args)
+
+def generate(fname, out, parser, templ_h, templ_c):
+
+    print("generating {0}".format(fname))
+
+    m, _ = os.path.splitext(os.path.basename(fname))
+
+    name_h = "%s_proto.h" % m
+    name_c = "%s_proto.c" % m
+
+    msgdef = parser.parse_from_filename(fname)
+
+    with open(os.path.join(out, name_h), 'w') as fp:
+        fp.write(templ_h.render(msgdef, version_major=sys.version_info.major))
+
+    with open(os.path.join(out, name_c), 'w') as fp:
+        fp.write(templ_c.render(msgdef, version_major=sys.version_info.major))
+
+
+if __name__ == "__main__":
+    main()
---
+++ setup.py
@@ -0,0 +1,124 @@
+from distutils.command.clean import clean as _clean
+from distutils.dir_util import remove_tree
+from distutils import log
+
+from setuptools import setup, find_packages, Distribution
+
+import os
+import os.path
+import platform
+import sys
+
+
+VERSION = "0.0.1"
+HERE = os.path.dirname(os.path.abspath(__file__))
+CYROBUF_LIST_H = "cyrobuf_list.h"
+CYROBUF_LIST_C = "cyrobuf_list.c"
+
+
+class clean(_clean):
+    def __clean_tree(self, tree):
+        for dirpath, dirnames, filenames in os.walk(tree):
+            for dirname in dirnames:
+                if dirname in ('__pycache__', 'build'):
+                    remove_tree(os.path.join(dirpath, dirname), verbose=self.verbose, dry_run=self.dry_run)
+
+        for dirpath, dirnames, filenames in os.walk(tree):
+            for filename in filenames:
+                if any(filename.endswith(suffix) for suffix in (".so", ".dll")):
+                    self.__remove_file(os.path.join(dirpath, filename))
+
+    def __remove_file(self, full_path):
+        if os.path.exists(full_path):
+            if not self.dry_run:
+                os.unlink(full_path)
+            if self.verbose >= 1:
+                log.info("removing '%s'" % full_path)
+
+    def run(self):
+        _clean.run(self)
+
+        self.__clean_tree(os.path.join(HERE, 'cyrobuf'))
+        self.__clean_tree(os.path.join(HERE, 'tests'))
+
+        tests_out = os.path.join(HERE, 'tests', 'out')
+        if os.path.isdir(tests_out):
+            remove_tree(tests_out, verbose=self.verbose, dry_run=self.dry_run)
+
+        self.__remove_file(os.path.join(HERE, 'cyrobuf', 'src', CYROBUF_LIST_H))
+        self.__remove_file(os.path.join(HERE, 'cyrobuf', 'src', CYROBUF_LIST_C))
+
+        for filename in os.listdir(HERE):
+            for prefix in ("cyrobuf_list", "cyrobuf_util"):
+                for suffix in (".so", ".dll"):
+                    if filename.startswith(prefix) and filename.endswith(suffix):
+                        self.__remove_file(os.path.join(HERE, filename))
+
+
+class PyrobufDistribution(Distribution):
+
+    def run_commands(self):
+        # By now the setup_requires deps have been fetched.
+        if not self.ext_modules:
+            self.ext_modules = list()
+        self.ext_modules.extend(self.cyrobufize_builtins())
+        Distribution.run_commands(self)
+
+    def cyrobufize_builtins(self):
+        from jinja2 import Environment, PackageLoader
+        env = Environment(loader=PackageLoader('cyrobuf.protobuf', 'templates'))
+
+        templ_h = env.get_template('cyrobuf_list_h.tmpl')
+        templ_c = env.get_template('cyrobuf_list_c.tmpl')
+
+        listdict = {
+            'DoubleList':   'double',
+            'FloatList':    'float',
+            'IntList':      'int',
+            'Int32List':    'int32_t',
+            'Uint32List':   'uint32_t',
+            'Int64List':    'int64_t',
+            'Uint64List':   'uint64_t',
+            'CharList':     'char'
+        }
+
+        path = os.path.join(HERE, 'cyrobuf', 'src', CYROBUF_LIST_C)
+        if not self.dry_run:
+            with open(path, 'w') as fp:
+                fp.write(templ_c.render(
+                    {'def': listdict, 'version_major': sys.version_info.major, 'format_map': format_map}
+                ))
+        if self.verbose >= 1:
+            log.info("rendering '%s' from '%s'" % (CYROBUF_LIST_C, templ_c.filename))
+
+        path = os.path.join(HERE, 'cyrobuf', 'src', CYROBUF_LIST_H)
+        if not self.dry_run:
+            with open(path, 'w') as fp:
+                fp.write(templ_h.render(
+                    {'def': listdict, 'version_major': sys.version_info.major, 'format_map': format_map}
+                ))
+        if self.verbose >= 1:
+            log.info("rendering '%s' from '%s'" % (CYROBUF_LIST_H, templ_h.filename))
+
+setup(
+    distclass=PyrobufDistribution,
+    name="cyrobuf",
+    version=VERSION,
+    packages=find_packages(),
+    include_package_data=True,
+    cmdclass={'clean': clean},
+    entry_points={
+        'console_scripts': ['cyrobuf = cyrobuf.__main__:main'],
+        'distutils.setup_keywords': [
+                'cyrobuf_modules = cyrobuf.setuptools_ext:cyrobuf_modules',
+        ],
+    },
+    description='A C based protobuf compiler',
+    long_description=open(os.path.join(HERE, 'README.md')).read(),
+    url='https://github.com/tburmeister/cyrobuf',
+    author='Taylor Burmeister',
+    tests_require=['pytest'] + (['protobuf >= 2.6.0, <3'] if sys.version_info.major == 2 else []),
+    setup_requires=['jinja2 >= 2.8', 'pytest-runner'],
+    install_requires=['jinja2 >= 2.8'],
+    zip_safe=False,
+)
