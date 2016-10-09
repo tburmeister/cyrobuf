b14d8aadb7683c17edc15798d0e96b0506937547
tburmeister <burmeister.taylor@gmail.com>
2016-10-09 12:21:15 -0400

    fixed a bug in parsing sub messages and fixed example buffer - example is now parsed correctly

--- tests/test.c
+++ tests/test.c
@@ -3,6 +3,20 @@
 
 #include "test_message_proto.h"
 
+static char *test_buffer =
+    "\x08\x80\x89\x9a\x81\x02\x10\xc1S\x1a\tgo goats! \x00 d \xc8\x01 "
+    "\xac\x02 \x90\x03*A\x08\xb9`\x12\x05hello\x1a\x12\tH\xe1z\x14\xae.\x96@\x12\x07goodbye!\xd7"
+    "\xa3p=\n/v@*\r\tn\x86\x1b\xf0\xf9!\t@\x12\x02pi2\tsomething2\x17\x08\x80\x89\x9a\x81\x02\x10"
+    "\xd7\x08\x19\x8d\x97n\x12\x83\xc0\xf3?\"\x03"
+    "fooB\x17\x08\x80\x89\x9a\x81\x02\x10\xd7\x08\x19"
+    "\x8d\x97n\x12\x83\xc0\xf3?\"\x03"
+    "fooJ7\n\nwhat\'s up?\x12\x0cnothing much\x18\x18 W*\x17\x08"
+    "\x80\x89\x9a\x81\x02\x10\xd7\x08\x19\x8d\x97n\x12\x83\xc0\xf3?\"\x03"
+    "fooP\xee\x87\xfb\xff\xff"
+    "\xff\xff\xff\xff\x01X\xff\xff\xff\xff\xff\xff\xff\xff\xff\x01";
+
+static size_t buffer_size = 229;
+
 int main(int argc, char **argv)
 {
 	struct Test *test = Test_init();
@@ -26,6 +40,16 @@ int main(int argc, char **argv)
     offset = 0;
     int64_t value = get_varint64(buffer, &offset);
     printf("got value %lld; consumed %zd bytes\n", value, offset);
+
+    offset = 0;
+    memcpy(buffer, test_buffer, buffer_size);
+
+    test = Test_init();
+    Test_parse_from_string(test, buffer, buffer_size, &offset);
+    Test_print(test, 0);
+    printf("\n");
+
+	Test_destroy(test);
 
 	return 0;
 }
